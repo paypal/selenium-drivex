4badea41c3780bb8b680e1494d587d84038e1316
Matt Edelman <medelman@paypal.com>
2015-03-05 13:25:36 -0800

    remove extraneous

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var wd = require('selenium-webdriver');
+var log = debug('selenium-drivex');
+
 
 module.exports = function drivex(driver) {
   var methods = {
@@ -30,14 +32,8 @@ module.exports = function drivex(driver) {
      * @returns {Promise} resolves to true or rejected
      */
     visible: function (locator, el) {
-      return (el ? el : driver).isElementPresent(locator).then(function(isPresent) {
-        if (isPresent) {
-          return methods.find(locator, el).then(function(elt) {
-            return elt.isDisplayed();
-          })
-        } else {
-          return false;
-        }
+      return methods.find(locator, el).then(function(elt) {
+        return elt.isDisplayed();
       });
     },
     /**
@@ -57,179 +53,6 @@ module.exports = function drivex(driver) {
         log(err.stack);
         throw new Error(msg || '[drivex.waitForElement] Element not locatable');
       });
-    },
-    /**
-     * determine if only one of the WebElements in the elements array is visible
-     * @param elements {Array} array of WebElements
-     * @returns {Promise} promise resolves to single visible element from "elements" or Error
-     */
-    oneDisplayed: function (elements) {
-      var length = elements.length,
-        current = 0,
-        displayedElt = null,
-        returnError = new Error("[drivex.oneDisplayed] Didn't find any displayed elements"),
-        d = wd.promise.defer();
-      if (!length) {
-        d.reject(new Error("[drivex.oneDisplayed] No WebElements passed in"));
-      }
-      async.whilst(function () {
-        return current < length;
-      }, function (callback) {
-        elements[current].isDisplayed().
-          then(function (disp) {
-            if (disp && displayedElt === null) {
-              displayedElt = elements[current];
-              returnError = null;
-            } else if (disp && displayedElt !== null) {
-              returnError = new Error("[drivex.oneDisplayed] Found more than one displayed element");
-            }
-            current++;
-            callback(returnError);
-          })
-      }, function (err) {
-        if (err) {
-          d.reject(err);
-        } else {
-          d.fulfill(displayedElt);
-        }
-      });
-      return d.promise;
-    },
-    /**
-     * Determine if all WebElements in elements array are enabled
-     * @param elements {Array} array of WebElements
-     * @returns {Promise} promise resolves to true or Error
-     */
-    allEnabled: function (elements) {
-      var length = elements.length,
-        current = 0,
-        returnError = null,
-        d = wd.promise.defer();
-      if (!length) {
-        d.reject(new Error("[drivex.allEnabled] No WebElements passed in"));
-      }
-      async.whilst(function () {
-        return current < length;
-      }, function (callback) {
-        elements[current].isEnabled().
-          then(function (enabled) {
-            if (!enabled) {
-              returnError = new Error("[drivex.allEnabled] All elements not enabled");
-            }
-            callback(returnError);
-            current++;
-          })
-      }, function (err) {
-        if (err) {
-          d.reject(err);
-        } else {
-          d.fulfill(true);
-        }
-      });
-      return d.promise;
-    },
-    /**
-     * Determine if all WebElements in elements array are disabled
-     * @param elements {Array} array of WebElements
-     * @returns {Promise} promise resolves to true or Error
-     */
-    allDisabled: function (elements) {
-      var length = elements.length,
-        current = 0,
-        returnError = null,
-        d = wd.promise.defer();
-      if (!length) {
-        d.reject(new Error("[drivex.allDisabled] No WebElements passed in"));
-      }
-      async.whilst(function () {
-        return current < length;
-      }, function (callback) {
-        elements[current].isEnabled().
-          then(function (enabled) {
-            if (enabled) {
-              returnError = new Error("[drivex.allDisabled] All elements not disabled");
-            }
-            callback(returnError);
-            current++;
-          })
-      }, function (err) {
-        if (err) {
-          d.reject(err);
-        } else {
-          d.fulfill(true);
-        }
-      });
-      return d.promise;
-    },
-    /**
-     * Determine if all WebElements in elements array are visible
-     * @param elements {Array} array of WebElements
-     * @returns {Promise} promise resolves to true or Error
-     */
-    allDisplayed: function (elements) {
-      var length = elements.length,
-        current = 0,
-        returnError = null,
-        d = wd.promise.defer();
-      if (!length) {
-        d.reject(new Error("[drivex.allDisplayed] No WebElements passed in"));
-      }
-      async.whilst(function () {
-        return current < length;
-      }, function (callback) {
-        //console.log("allDisplayed, current", current);
-        elements[current].isDisplayed().
-          then(function (displayed) {
-            //console.log("displayed",displayed);
-            if (!displayed) {
-              returnError = new Error("[drivex.allDisplayed] All elements not displayed");
-            }
-
-            current++;
-            callback(returnError);
-          })
-      }, function (err) {
-        if (err) {
-          d.reject(err);
-        } else {
-          d.fulfill(true);
-        }
-      });
-      return d.promise;
-    },
-    /**
-     * Determine if all WebElements in elements array are hidden
-     * @param elements {Array} array of WebElements
-     * @returns {Promise} promise resolves to true or Error
-     */
-    allHidden: function (elements) {
-      var length = elements.length,
-        current = 0,
-        returnError = null,
-        d = wd.promise.defer();
-      if (!length) {
-        d.reject(new Error("[drivex.allHidden] No WebElements passed in"));
-      }
-      async.whilst(function () {
-        return current < length;
-      }, function (callback) {
-        elements[current].isDisplayed().
-          then(function (displayed) {
-            if (displayed) {
-              returnError = new Error("[drivex.allHidden] All elements not hidden");
-            }
-            current++;
-            callback(returnError);
-
-          })
-      }, function (err) {
-        if (err) {
-          d.reject(err);
-        } else {
-          d.fulfill(true);
-        }
-      });
-      return d.promise;
     }
   };
   return methods;
